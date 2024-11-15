9dd1566c812b5cba27dbaf0246fa1ada361c5241
Olena <poholska@gmail.com>
2024-11-15 00:40:17 -0600

    finish task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,24 +1,17 @@
 function getShippingCost(country) {
     let price;
 
-
-    switch (country) {
-    case "China":
-        price = 100;
-        break;
-    case "Chile":
-        price = 250;
-        break;
-    case "Australia":
-        price = 170;
-        break;
-    case "Jamaica":
-        price = 120;
-        break;
-    default:
-        return "Sorry, there is no delivery to your country";
+    if (country === "China") {
+    price = 100;
+    } else if (country === "Chile") {
+    price = 250;
+    } else if (country === "Australia") {
+    price = 170;
+    } else if (country === "Jamaica") {
+    price = 120;
+    } else {
+    return `Sorry, there is no delivery to your country`;
     }
-
     return `Shipping to ${country} will cost ${price} credits`;
 }
 
